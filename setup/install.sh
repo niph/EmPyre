cfbaa367e10b2214133bb8ebe920c4cc56610168
niph <niph.dev@gmail.com>
2016-05-22 20:26:17 +0200

    added OSX support

--- setup/install.sh
+++ setup/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo " [!]This script must be run as root" 1>&2
-   exit 1
-fi
 
 IFS='/' read -a array <<< pwd
 
@@ -12,56 +8,88 @@ then
     cd ./setup
 fi
 
-version=$( lsb_release -r | grep -oP "[0-9]+" | head -1 )
-if lsb_release -d | grep -q "Fedora"; then
-	Release=Fedora
-	dnf install -y python-devel m2crypto python-m2ext swig python-iptools python3-iptools 
-	pip install pycrypto
-	pip install iptools
-	pip install pydispatcher
-	pip install macholib
-	pip install flask
-elif lsb_release -d | grep -q "Kali"; then
-	Release=Kali
-	apt-get install python-dev
-	apt-get install python-m2crypto
-	apt-get install swig
-	apt-get install python-pip
-	pip install pycrypto
-	pip install iptools
-	pip install pydispatcher
-	pip install macholib
-	pip install flask
-elif lsb_release -d | grep -q "Ubuntu"; then
-	Release=Ubuntu
-	apt-get install python-dev
-	apt-get install python-m2crypto
-	apt-get install swig
-	pip install pycrypto
-	pip install iptools
-	pip install pydispatcher
-	pip install macholib
-	pip install flask
+if [[ "uname -a | grep Darwin" ]]
+then
+	if [ -x $(which brew) ]
+	then
+		brew install openssl
+		brew install swig
+		brew install python
+		env LDFLAGS="-L$(brew --prefix openssl)/lib" \
+			CFLAGS="-I$(brew --prefix openssl)/include" \
+			SWIG_FEATURES="-cpperraswarn -includeall -I$(brew --prefix openssl)/include" \
+		pip install m2crypto
+		pip install cryptography
+		pip install pycrypto
+		pip install iptools
+		pip install pydispatcher
+		pip install macholib
+		pip install flask
+	else
+		echo "[!] Homebrew not found - please install homebrew and try again"
+		exit 1
+	fi
 else
-	echo "Unknown distro - Debian/Ubuntu Fallback"
-	 apt-get install python-dev
-	 apt-get install python-m2crypto
-	 apt-get install swig
-	 pip install pycrypto
-	 pip install iptools
-	 pip install pydispatcher
-	 pip install macholib
-	 pip install flask
-fi
-
+	if [[ $EUID -ne 0 ]]; then
+	   echo " [!]This script must be run as root" 1>&2
+	   exit 1
+	fi
+	version=$( lsb_release -r | grep -oP "[0-9]+" | head -1 )
+	if lsb_release -d | grep -q "Fedora"; then
+		Release=Fedora
+		dnf install -y python-devel m2crypto python-m2ext swig python-iptools python3-iptools 
+		pip install pycrypto
+		pip install iptools
+		pip install pydispatcher
+		pip install macholib
+		pip install flask
+	elif lsb_release -d | grep -q "Kali"; then
+		Release=Kali
+		apt-get install python-dev
+		apt-get install python-m2crypto
+		apt-get install swig
+		apt-get install python-pip
+		pip install pycrypto
+		pip install iptools
+		pip install pydispatcher
+		pip install macholib
+		pip install flask
+	elif lsb_release -d | grep -q "Ubuntu"; then
+		Release=Ubuntu
+		apt-get install python-dev
+		apt-get install python-m2crypto
+		apt-get install swig
+		pip install pycrypto
+		pip install iptools
+		pip install pydispatcher
+		pip install macholib
+		pip install flask
+	else
+		echo "Unknown distro - Debian/Ubuntu Fallback"
+		apt-get install python-dev
+		apt-get install python-m2crypto
+		apt-get install swig
+		pip install pycrypto
+		pip install iptools
+		pip install pydispatcher
+		pip install macholib
+		pip install flask
+	fi
+fi 
 # set up the database schema
-./setup_database.py
+python setup_database.py
 
 # generate a cert
 ./cert.sh
 
 # Support non-root instalation of DB
-chown $(logname):$(logname) ../data/empyre.db
+if [[ "uname -a | grep Darwin" ]]
+then 
+	GROUP="staff"
+else
+	GROUP=$(logname)
+fi
+chown $(logname):$GROUP ../data/empyre.db
 
 cd ..
 
